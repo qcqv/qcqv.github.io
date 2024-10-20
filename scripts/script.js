54008e8baeabdb91deaba9722d9b3b3b2d04b603
qcqv <120058865+qcqv@users.noreply.github.com>
2024-10-20 18:40:47 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,25 @@ document.addEventListener('DOMContentLoaded', function() {
         video.volume = volume;
     });
 });
+
+document.getElementById('click-area').addEventListener('click', function() {
+    const hiddenContent = document.getElementById('hidden-content');
+    hiddenContent.style.display = 'block';
+    const centerText = document.getElementById('center-text');
+    centerText.style.display = 'none';
+
+    // Play the audio
+    const audio = document.getElementById('audio-background');
+    audio.muted = false;
+    audio.play();
+});
+
+// Volume control
+document.addEventListener('DOMContentLoaded', function() {
+    const volumeSlider = document.getElementById('volume-slider');
+    volumeSlider.addEventListener('input', function() {
+        const volume = this.value;
+        const audio = document.getElementById('audio-background');
+        audio.volume = volume;
+    });
+});
